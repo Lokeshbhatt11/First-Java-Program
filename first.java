0ef5b18cdce33014e836cefb67c56e9ec1eb230a
Lokesh Bhatt <89529833+Lokeshbhatt11@users.noreply.github.com>
2024-05-07 10:35:21 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package snippet;
+
+/**
+ * @author user
+ *
+ */
+public class src {
+
+	/**
+	 * 
+	 */
+	public src() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String name = "Hello World" ;
+		system.println.out(name);
+	}
+
+}
